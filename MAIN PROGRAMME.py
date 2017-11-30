ae27341e9ad763ede6572db9ae31a63f5f2d5a2e
Denis Gavrielov <d.gavrielov@icloud.com>
2017-11-30 09:08:06 +0000

    updated description

--- MAIN PROGRAMME.py
+++ MAIN PROGRAMME.py
@@ -1,7 +1,7 @@
 '''
 27/09/2017
 
-This programme is designed to help me speed up my US stock trading research by 
+This program is designed to help me speed up my US stock trading research by 
 automating web searches through web scraping methods. 
 It asks for a US stock ticker and returns the stock's chart (in different time 
 frames), key financials, recent SEC quarterly/yearly reports and  
@@ -10,9 +10,9 @@ the three most recent news stories and press releases found on finance.yahoo.
 It prints the progress on scraping finance.yahoo in the text editor. After 10 attempts 
 to request the news stories or press releases it will skip this task. 
 
-Note that the programme is done for my productivity and not to look fancy. 
+Note that the program is done for my productivity and not to look fancy. 
 
-If you want to reach out or are testing this programme and have any recommendations 
+If you want to reach out or are testing this program and have any recommendations 
 then please let me know via email (d.gavrielov@icloud.com). 
 I am always happy to improve myself. :)
 '''
